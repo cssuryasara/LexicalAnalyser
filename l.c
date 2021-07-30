ad95d2475062593d044bdc1bb31ed0532e553dec
unknown <malathisara123@gmail.com>
2021-07-30 14:00:20 +0530

    final commit

--- l.c
+++ l.c
@@ -1,16 +1,15 @@
-//Addition of two number
-/**
-  * This Program Add two numbers.
-  */
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-  /*
-   * This program add two integer numbers.
-   */
-  int a = 5, b = 10, c;
-  c = a+b * 1c;
-  printf("Sum = %d\n", c);
-  return 0;
+int main(){
+  float a;
+  a = 1.1;
+  int i, inc, j;
+  i = 0;
+  j=2;
+  inc = 2;
+  while (i < j){
+    i = i + inc;
+    a = a*i;
+  }
+  if (a > 10.0){
+    printf(a + i);
+  }
 }
